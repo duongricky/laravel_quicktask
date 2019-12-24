dfba53331aa3b480e60460691d91a4bee0f9cfbf
duongricky <duongricky113@gmail.com>
2019-12-24 09:51:56 +0700

    add migration,model,routes for tasks

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,26 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
+/**
+ * Show Task Dashboard
+ */
 Route::get('/', function () {
-    return view('welcome');
+    return view('task');
+});
+
+/**
+ * Add New Task
+ */
+Route::post('/task', function (Request $request) {
+    //
+});
+
+/**
+ * Delete Task
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    //
 });
